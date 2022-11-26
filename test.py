0cd25930d2cbd948692ca3e00ad18b4bc05bca20
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2022-11-26 10:12:00 +0900

    か

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import gensim
 model = gensim.models.Word2Vec.load('ja.bin')
-for item, value in model.wv.most_similar('島村'):
+for item, value in model.wv.most_similar('島'):
     print(item,value)
 
+
+sim=model.wv.similarity('幸せ', '幸福')
+print(sim)
